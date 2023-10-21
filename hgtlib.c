27dd1100ef20bec8c6321f2f6106b96856f77e0e
Moldytzu <m.alex123@yahoo.com>
2023-10-21 20:05:28 +0300

    create a context system

--- hgtlib.c
+++ hgtlib.c
@@ -2,35 +2,64 @@
 #include <stdlib.h>
 #include <stdint.h>
 
-const char *file = "N48E029.hgt"; // fixme: don't hard-code
-FILE *fd;
+typedef struct
+{
+    const char *filename;
+    FILE *fd;
+    uint16_t *buffer;
+    uint32_t bufferSize;
+} hgt_file_t;
 
-uint16_t readShort(size_t offset)
+static const uint32_t lines = 1201, samples = 1201; // SRTM3
+
+hgt_file_t *hgtCreateContext(const char *filename)
+{
+    hgt_file_t *hgt = (hgt_file_t *)malloc(sizeof(hgt_file_t));
+
+    if (!(hgt->fd = fopen(filename, "rb"))) // open file as binary
+    {
+        printf("Failed to load file %s\n", filename);
+        return NULL;
+    }
+
+    fseek(hgt->fd, 0, SEEK_END);      // seek at the end
+    hgt->bufferSize = ftell(hgt->fd); // read the file pointer
+    fseek(hgt->fd, 0, SEEK_SET);      // seek at the start
+
+    hgt->buffer = malloc(hgt->bufferSize);           // allocate a buffer to hold the buffer
+    fread(hgt->buffer, hgt->bufferSize, 1, hgt->fd); // read the whole file
+
+    return hgt;
+}
+
+void hgtDeleteContext(hgt_file_t *hgt)
+{
+    // close the file
+    fclose(hgt->fd);
+
+    // delete the used pointers
+    free(hgt->buffer);
+    free(hgt);
+}
+
+int16_t hgtReadElevationRaw(hgt_file_t *hgt, size_t offset)
 {
     uint16_t be, le;
-    fseek(fd, offset * sizeof(uint16_t), SEEK_SET);
-    fread(&be, sizeof(uint16_t), 1, fd); // read a short in our requested variable
 
+    be = hgt->buffer[offset];            // read the elevation value at the offset
     le = ((be & 0xFF) << 8) | (be >> 8); // swap endianness (x86_64 is little-endian)
                                          // fixme: don't assume this is x86_64!
 
-    return le;
+    return (int16_t)le;
 }
 
+// fixme: move this in a test.c file
 int main()
 {
-    fd = fopen(file, "rb"); // open file as binary
+    hgt_file_t *hgt = hgtCreateContext("N48E029.hgt"); // fixme: don't hard-code
 
-    if (!fd)
-    {
-        printf("Failed to load file %s\n", file);
-        return 0;
-    }
-
-    for (int i = 0; i < 16; i++)
-        printf("%x ", readShort(i));
-
-    fclose(fd); // close our file
+    for (int i = 0; i < 1201; i++)
+        printf("%d ", hgtReadElevationRaw(hgt, i));
 
     return 0;
 }
